6af5b38d09f665abaa330a2698d0e62bdc5a0c30
usha <126246565+usharani222@users.noreply.github.com>
2025-08-21 21:43:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (75.1%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+     int x=0,y=0;
+     for(char i:moves)
+     {
+        if(i=='U') y++;
+        else if(i=='D') y--;
+        else if(i=='R') x++;
+        else x--;
+     }   
+     return (x==0&&y==0);
+    }
+};
