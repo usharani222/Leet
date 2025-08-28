5bb72d09fa70b380e2518f4438dee669c104b816
usha <126246565+usharani222@users.noreply.github.com>
2025-08-28 10:36:32 +0530

    Time: 44 ms (12.43%), Space: 22.3 MB (39.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,10 +3,17 @@ public:
     void dfs(vector<vector<char>>& grid,int i,int j)
     {
         grid[i][j]='2';
-        if(i>0&&grid[i-1][j]=='1')    dfs(grid,i-1,j);
-        if(i<grid.size()-1&&grid[i+1][j]=='1')    dfs(grid,i+1,j);
-        if(j>0&&grid[i][j-1]=='1')    dfs(grid,i,j-1);
-        if(j<grid[0].size()-1&&grid[i][j+1]=='1')   dfs(grid,i,j+1);
+        // if(i>0&&grid[i-1][j]=='1')    dfs(grid,i-1,j);
+        // if(i<grid.size()-1&&grid[i+1][j]=='1')    dfs(grid,i+1,j);
+        // if(j>0&&grid[i][j-1]=='1')    dfs(grid,i,j-1);
+        // if(j<grid[0].size()-1&&grid[i][j+1]=='1')   dfs(grid,i,j+1);
+        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        for(auto [dx,dy]:dir)
+        {
+            int ni=i+dx,nj=j+dy;
+            if(ni>=0 && ni<grid.size() && nj>=0 && nj<grid[0].size() && grid[ni][nj]=='1') 
+                dfs(grid,ni,nj);
+        }
         return;
     }
     int numIslands(vector<vector<char>>& grid) 
