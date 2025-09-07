cb84c51e5e2fc70a21e972e1652432302487f002
usha <126246565+usharani222@users.noreply.github.com>
2025-09-07 12:38:27 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> ans;
-        int c=0;
-        for(int i:nums)
+        // vector<int> ans;
+        // int c=0;
+        // for(int i:nums)
+        // {
+        //     if(i!=0) ans.push_back(i);
+        //     else c++;
+        // }
+        // while(c--)  ans.push_back(0);
+        // nums=ans;
+
+
+        int prev=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(i!=0) ans.push_back(i);
-            else c++;
+            if(nums[i]!=0)      nums[prev++]=nums[i];
         }
-        while(c--)  ans.push_back(0);
-        nums=ans;
-
-
-        // int prev=0;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     if(nums[i]!=0)      nums[prev++]=nums[i];
-        // }
-        // for(int j=prev;j<nums.size();j++)
-        // {
-        //     nums[j]=0;
-        // }
+        for(int j=prev;j<nums.size();j++)
+        {
+            nums[j]=0;
+        }
     }
 };
