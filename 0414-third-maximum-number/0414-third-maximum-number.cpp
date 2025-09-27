7d2e0a819980671e36af67bf3a347940e97b4d3e
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-09-27 14:02:30 +0530

    Time: 0 ms (100%), Space: 12.8 MB (93.42%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) 
     {
-        // int max1=-1,max2=-1,max3=-1;
-        // for(int i:nums)
-        // {
-        //     if(i>max1)
-        //     {
-        //         max3=max2;
-        //         max2=max1;
-        //         max1=i;
-        //     }
-        //     else if(i<max1&&i>max2)
-        //     {
-        //         max3=max2;
-        //         max2=i;
-        //     }
-        //     else if(i<max2&&i>max3) max3=i;
-        // }
-        // return max3==-1?max1:max3;
-        set<int> s(nums.begin(),nums.end());
-        priority_queue<int> p;
-        for(int i:s) p.push(i);
-        if(p.size()<3) return p.top();
-        p.pop();
-        p.pop();
-        return p.top();
+        long max1=LONG_MIN,max2=LONG_MIN,max3=LONG_MIN;
+        for(long i:nums)
+        {
+            if(i>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=i;
+            }
+            else if(i<max1&&i>max2)
+            {
+                max3=max2;
+                max2=i;
+            }
+            else if(i<max2&&i>max3) max3=i;
+        }
+        // if(nums.size()<3) return max1;
+        return max3==LONG_MIN?max1:max3;
+        // set<int> s(nums.begin(),nums.end());
+        // priority_queue<int> p;
+        // for(int i:s) p.push(i);
+        // if(p.size()<3) return p.top();
+        // p.pop();
+        // p.pop();
+        // return p.top();
     }
 };
