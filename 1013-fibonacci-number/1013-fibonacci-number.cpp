05fb0a009000e82b75ef5d4a081d5963f850043f
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-09-27 12:53:08 +0530

    Time: 3 ms (66.58%), Space: 8.2 MB (11.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    int fibnocii(int n,vector<int> &dp)
+    {
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fibnocii(n-1,dp)+fibnocii(n-2,dp);
+    }
     int fib(int n) 
     {
-        if(n==0||n==1) return n;
-        return fib(n-1)+fib(n-2);    
+        // recurssion
+        // if(n==0||n==1) return n;
+        // return fib(n-1)+fib(n-2);    
+
+        //dp
+        vector<int> dp(n+1,-1);
+        if(n<=1) return n;
+        dp[0]=0,dp[1]=1;
+        fibnocii(n,dp);
+        return dp[n];
     }
 };
