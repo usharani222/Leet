10c45cd3d1fbfc81626d45446f9265bbe2d5a58d
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-09-28 11:51:58 +0530

    Time: 0 ms (100%), Space: 28.2 MB (63.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) 
-    {
-        map<int,int> m;
-        for(int i:nums) m[i]++;
-        int n=nums.size();
-        for(auto i:m)
+    int majorityElement(vector<int>& nums) {
+        int num=nums[0],count=0;
+        for(int i:nums)
         {
-            if(i.second>n/2) return i.first;
-        }    
-        return -1;
+            if(num==i)  count++;
+            else if(count==0)   num=i;
+            else count--;
+        }
+        return num;
     }
 };
+
+//Boyer-Moore Majority Voting Algorithm
+// Initialization:
+
+// The element variable is initialized to the first element of the array (nums[0]).
+// The count is initialized to 0, which will keep track of how many times the current element appears as we iterate through the array.
+// Iteration through the array:
+// The loop iterates over the entire array nums[], and for each number:
+
+// If the current number (nums[i]) matches the element, the count is incremented.
+// If the current number doesn't match element, and count is 0, we set the element to nums[i] (switch to a new candidate) and reset count to 1.
+// If the current number doesn't match element and count is not 0, we decrement the count.
+// Return the majority element:
+// After iterating through the entire array, the element variable will hold the candidate for the majority element, and that is returned.
