553d9aee2cfe36c54ac77df770467a46928db800
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-09-29 22:08:17 +0530

    Time: 6 ms (47.55%), Space: 23.9 MB (57.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,11 +4,12 @@ public:
     {
         vector<vector<int>> v;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size();i++)
+        v.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++)
         {
-            if(!v.empty()&&v.back()[1]>=intervals[i][0]) v.back()[1]=max(v.back()[1],intervals[i][1]);
+            if(v.back()[1]>=intervals[i][0]) v.back()[1]=max(v.back()[1],intervals[i][1]);
             else v.push_back(intervals[i]);
-        }   
+        }
         return v;
     }
 };
