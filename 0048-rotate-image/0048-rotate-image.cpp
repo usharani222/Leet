48e7821fbfd99ad816d2bbbc941bcae3520193ba
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-09-29 08:31:44 +0530

    Time: 0 ms (100%), Space: 10.3 MB (9.37%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         swap(matrix[i][j],matrix[j][i]);
-        //     }
-        // }   
-        // for(int i=0;i<n;i++)
-        // {
-        //     reverse(matrix[i].begin(),matrix[i].end());
-        // }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }   
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
 
-        vector<vector<int>> ans(n,vector<int>(n));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                ans[j][n-i-1]=matrix[i][j];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                matrix[i][j]=ans[i][j];
-            }
-        }
+        // vector<vector<int>> ans(n,vector<int>(n));
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         ans[j][n-i-1]=matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         matrix[i][j]=ans[i][j];
+        //     }
+        // }
 
     }
 };
