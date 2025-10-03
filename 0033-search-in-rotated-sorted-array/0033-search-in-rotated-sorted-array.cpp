6579607442a18891f486fe7d06eaa5f4c89e4418
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-10-03 18:20:00 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
+    // int bs(int l,int h,vector<int>& nums,int x)
+    // {
+    //     while(l<=h)
+    //     {
+    //         int mid=l+(h-l)/2;
+    //         if(nums[mid]==x) return mid;
+    //         else if(nums[l]<=nums[mid])
+    //         {
+    //             if(nums[l]<=x&&x<=nums[mid]) h=mid-1;
+    //             else l=mid+1;
+    //         }
+    //         else
+    //         {
+    //             if(nums[mid]<=x&&x<=nums[h]) l=mid+1;
+    //             else h=mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // int search(vector<int>& nums, int target) 
+    // {
+    //     return bs(0,nums.size()-1,nums,target);    
+    // }
     int bs(int l,int h,vector<int>& nums,int x)
     {
-        while(l<=h)
+        if(l>h) return -1;
+        int mid=l+(h-l)/2;
+        if(nums[mid]==x) return mid;
+        else if(nums[l]<=nums[mid])
         {
-            int mid=l+(h-l)/2;
-            if(nums[mid]==x) return mid;
-            else if(nums[l]<=nums[mid])
-            {
-                if(nums[l]<=x&&x<=nums[mid]) h=mid-1;
-                else l=mid+1;
-            }
-            else
-            {
-                if(nums[mid]<=x&&x<=nums[h]) l=mid+1;
-                else h=mid-1;
-            }
+            if(nums[l]<=x&&x<nums[mid]) return bs(l,mid-1,nums,x);
+            else return bs(mid+1,h,nums,x);
+        }
+        else
+        {
+            if(nums[mid]<x&&x<=nums[h]) return bs(mid+1,h,nums,x);
+            else return bs(l,mid-1,nums,x);
         }
         return -1;
     }
