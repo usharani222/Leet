0bf9c96bbe0bb8a2d8ec2f011168d2fa623099e0
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-10-29 11:18:23 +0530

    Time: 203 ms (2.57%), Space: 8.1 MB (89.59%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isset(int n)
+    {
+        while(n>0)
+        {
+            if(n%2==0) return false;
+            n/=2;
+        }
+        return true;
+    }
+    int smallestNumber(int n) {
+        for(int i=n;i<INT_MAX;i++)  
+        {
+            if(isset(i)) return i;
+            cout<<i<<endl;
+        }
+        return 0;
+    }
+};
