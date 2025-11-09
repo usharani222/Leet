8b0ad3fe0a662f97f09bf12468c7139edb84d763
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-11-09 15:17:29 +0530

    Time: 0 ms (100%), Space: 9.3 MB (62.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,15 +3,13 @@ public:
     int uniquePaths(int m, int n) 
     {
         vector<vector<int>> v(m,vector<int>(n,1));
-        // for(int i=0;i<n;i++) v[m-1][i]=1;
-        // for(int i=0;i<m;i++) v[i][n-1]=1; 
         for(int i=m-2;i>=0;i--)
         {
             for(int j=n-2;j>=0;j--)
             {
                 v[i][j]=v[i+1][j]+v[i][j+1];
             }
-        } 
+        }
         return v[0][0];
     }
 };
