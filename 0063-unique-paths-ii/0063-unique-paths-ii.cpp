f877bfedd064079a05a2c91281d77b215d28de70
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-11-09 15:42:46 +0530

    Time: 0 ms (100%), Space: 11.9 MB (27.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) 
+    int fun(vector<vector<int>> &v,int m,int n,int i,int j)
     {
-        int m=grid.size(),n=grid[0].size();
+        if(i>=m||j>=n) return 0;
+        if(v[i][j]==1) return 0;
+        if(i==m-1&&j==n-1) return 1;
+        int right=fun(v,m,n,i+1,j);
+        int down=fun(v,m,n,i,j+1);
+        return right+down;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+    {
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        // return fun(obstacleGrid,m,n,0,0);
+        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) return 0;
+        vector<vector<int>> v(m,vector<int>(n));
+        v[0][0]=1;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==1) grid[i][j]=-1;
+                if(obstacleGrid[i][j]==1) v[i][j]=0;
+                else
+                {
+                    if(i>0) v[i][j]+=v[i-1][j];
+                    if(j>0) v[i][j]+=v[i][j-1];
+                }
             }
         }
-        if(grid[m-1][n-1]==-1) return 0;
-        if(grid[0][0]==-1) return 0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(grid[m-1][i]==-1) break;
-            grid[m-1][i]=1;
-        }
-        for(int i=m-1;i>=0;i--)
-        {
-            if(grid[i][n-1]==-1) break;
-            grid[i][n-1]=1;
-        }
-        for(int i=m-2;i>=0;i--)
-        {
-            for(int j=n-2;j>=0;j--)
-            {
-                if(grid[i][j]==-1) continue;
-                long long down=(grid[i+1][j]==-1)?0:grid[i+1][j];
-                long long right=(grid[i][j+1]==-1)?0:grid[i][j+1];
-                grid[i][j]=down+right;
-            }
-        }
-        return grid[0][0];
+        return v[m-1][n-1];
     }
 };
