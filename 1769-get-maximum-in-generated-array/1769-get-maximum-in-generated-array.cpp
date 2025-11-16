92b4b99bda4b73094f273aef47f59f6c04a6ac28
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-11-16 14:47:51 +0530

    Time: 13 ms (0.57%), Space: 9.1 MB (9.74%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
@@ -5,10 +5,11 @@ public:
         if(n==1) return 1;
         vector<int> v(n+1);
         v[0]=0;v[1]=1;
-        for(int i=1;2*i<=n;i++)
+        for(int i=1;2*i+1<=n;i++)
         {
             v[2*i]=v[i];
-            if(2*i+1<=n) v[2*i+1]=v[i]+v[i+1];
+            // if(2*i+1<=n) 
+            v[2*i+1]=v[i]+v[i+1];
         }
         for(int i:v) cout<<i<<' ';
         return *max_element(v.begin(),v.end());
