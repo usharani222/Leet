91dc949f3159c6eeaf6d28f793a182d5b0f6c919
Usha Rani G <126246565+usharani222@users.noreply.github.com>
2025-11-20 20:38:43 +0530

    Time: 5 ms (18.34%), Space: 15.8 MB (35.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void parenthesis(int open,int close,vector<string> &v,string s){
-        if(open==0&&close==0)   
+    void parenthesis(int open,int close,int n,string s,vector<string> &ans)
+    {
+        if(s.size()==2*n)
         {
-            v.push_back(s);
+            ans.push_back(s);
             return;
         }
-        if(open>0)
-        {
-            s.push_back('(');
-            parenthesis(open-1,close,v,s);
-            s.pop_back();
-        }
-        if(open<close)
-        {
-            s.push_back(')');
-            parenthesis(open,close-1,v,s);
-            s.pop_back();
-        }
+        if(open<n) parenthesis(open+1,close,n,s+'(',ans);
+        if(close<open) parenthesis(open,close+1,n,s+')',ans);
+        return;
     }
-    vector<string> generateParenthesis(int n) {
-        vector<string> v;
-        string s;
-        parenthesis(n,n,v,s); 
-        return v;   
+    vector<string> generateParenthesis(int n) 
+    {
+        vector<string> ans;
+        parenthesis(0,0,n,"",ans);
+        return ans;    
     }
 };
